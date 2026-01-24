3a73c5e5e59a483f7b546b134658daec0ea94ed4
Prakash Rajak <prakashrajak1807@gmail.com>
2026-01-24 13:09:44 +0530

    Initial Commit

---
+++ SwiftConcurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrency
+//
+//  Created by Prakash Rajak on 24/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Prakash Rajak on 24/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
