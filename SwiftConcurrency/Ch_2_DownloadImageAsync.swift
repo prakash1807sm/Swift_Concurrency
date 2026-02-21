93a5d1058f3305126ab36f1fe2e47f64fe0be6b7
Prakash Rajak <prakashrajak1807@gmail.com>
2026-02-21 20:32:41 +0530

    Ch_2 - download images with escaping closure, combine and async/await(swift concurrency)

---
+++ SwiftConcurrency/Ch_2_DownloadImageAsync.swift
@@ -0,0 +1,112 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrency
+//
+//  Created by Prakash Rajak on 21/02/26.
+//
+
+import SwiftUI
+import Combine
+
+class DownloadImageAsyncImageLoader {
+    
+    let url = URL(string: "https://picsum.photos/id/237/200/300")!
+    
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage?{
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+    
+    func downloadImageWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, nil)
+        }
+        .resume()
+    }
+    
+    func downloadWithCombine() -> AnyPublisher<UIImage?, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(handleResponse)
+            .mapError({ $0 })
+            .eraseToAnyPublisher()
+    }
+    
+    func downloadWithAsync() async throws -> UIImage? {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
+            return handleResponse(data: data, response: response)
+        } catch {
+            throw error
+        }
+    }
+}
+
+class DownloadImageAsyncViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    func fetchImage() async {
+        //escaping closure
+        /*
+        loader.downloadImageWithEscaping {[weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+         */
+        
+        //combine
+        /*
+        loader.downloadWithCombine()
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                
+            } receiveValue: { [weak self] image in
+                self?.image = image
+            }
+            .store(in: &cancellables)
+         */
+        
+        //async-await - swift concurrency
+        //await is a suspension point on a task. we are literally going to wait for the response.
+        let image = try? await loader.downloadWithAsync()
+        
+        await MainActor.run { //to run the below line on main thread. we can also do using DispatchQueue.main, but we should use MainActor in swift concurrency.
+            self.image = image
+        }
+    }
+}
+
+struct DownloadImageAsync: View {
+    @StateObject private var viewModel = DownloadImageAsyncViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
+        }
+        .onAppear {
+            //NOTE: async functions can only be called inside async enviroment.
+            Task { //Task makes the context asynchronous.
+                await viewModel.fetchImage()
+            }
+        }
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
